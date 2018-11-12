470a713c874ab100276befcc75e9c59cd5d69758
iukh <33911363+iukh@users.noreply.github.com>
2018-11-12 16:10:37 +0300

    add "buy" button handling

--- src/js/main.js
+++ src/js/main.js
@@ -3,9 +3,19 @@
         init: function() {
           },
         ClickContinueButton: function() {
-            $(".content__main-button").click(function() {
-              console.log(" button has been clicked");
-              $(".container__about-us-text").hide();
+            $(".buy_abonnement").click(function() {
+              $(".menu__item").removeClass("menu__item_active");
+              $("#abonnement-tab").toggleClass("menu__item_active");
+              $.ajax({
+                   url: "/blocks/about-us.html",
+                   dataType: "html",
+                   success: function(data) {
+                       $(".container").html(data);
+                   },
+                   error: function() {
+                       alert("Not OK");
+                   }
+                 });
             });
         }
     };
